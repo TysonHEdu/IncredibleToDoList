5187cefd3fdc3e62fc01373d458c9740d5deacfc
TysonHEdu <tyson.harder@edu.sait.ca>
2023-11-03 23:57:09 -0600

    Assignment 2 fix
    Missed the obvious

--- ToDoForm.js
+++ ToDoForm.js
@@ -10,3 +10,21 @@ export default function ToDoForm() {
     <Button title="Add" />
 </View>
 }
+
+const styles = StyleSheet.create({
+    form: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginHorizontal: 20,
+        marginTop: 20,
+    },
+    input: {
+        flex: 1,
+        borderWidth: 1,
+        borderColor: '#ccc',
+        paddingHorizontal: 10,
+        paddingVertical: 5,
+        marginRight: 10,
+    },
+});
--- ToDoList.js
+++ ToDoList.js
@@ -1,21 +1,38 @@
 import { Pressable, ScrollView, View, Text, StyleSheet } from "react-native";
 import React from 'react';
+
 export default function ToDoList() {
-<ScrollView>
-    <Pressable>
-        <View style={[styles.task, styles.completed]}>
-            <Text style={styles.taskText}>Do laundry</Text>
-        </View>
-    </Pressable>
-    <Pressable>
-        <View style={[styles.task]}>
-            <Text style={styles.taskText}>Go to gym</Text>
-        </View>
-    </Pressable>
-    <Pressable>
-        <View style={[styles.task, styles.completed]}>
-            <Text style={styles.taskText}>Walk dog</Text>
-        </View>
-    </Pressable>
-</ScrollView>
+    return (
+        <ScrollView>
+            <Pressable>
+                <View style={[customStyles.task, customStyles.completed]}>
+                    <Text style={customStyles.taskText}>Do laundry</Text>
+                </View>
+            </Pressable>
+            <Pressable>
+                <View style={[customStyles.task]}>
+                    <Text style={customStyles.taskText}>Go to gym</Text>
+                </View>
+            </Pressable>
+            <Pressable>
+                <View style={[customStyles.task, customStyles.completed]}>
+                    <Text style={customStyles.taskText}>Walk dog</Text>
+                </View>
+            </Pressable>
+        </ScrollView>
+    );
 }
+
+const customStyles = StyleSheet.create({
+    task: {
+        padding: 10,
+        borderBottomWidth: 1,
+        borderColor: '#ccc',
+    },
+    completed: {
+        backgroundColor: '#e0e0e0',
+    },
+    taskText: {
+        fontSize: 16,
+    },
+});
